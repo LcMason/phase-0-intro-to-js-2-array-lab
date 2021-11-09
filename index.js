90c38ba9b3e426885d0c45fe2de0a930219c0a2a
Lionel Mason <capitalcookhouse@gmail.com>
2021-11-08 20:38:47 -0500

    commit tree ee6567ef61844c65c8072babe17b085dd2fdd403

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph){
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(Bob){
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(Ralph){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+ function appendCat(Broom){
+     return [...cats, 'Broom'];
+ }
+
+ function prependCat(Arnold){
+     return ['Arnold', ...cats];
+ }
+
+ function removeFirstCat(){
+     cats.slice(1);
+ }
